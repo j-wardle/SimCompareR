06b90f63b22aadfde86e6dedac3ade65a2574350
Jack Wardle <j.wardle19@imperial.ac.uk>
2021-01-05 15:21:49 +0000

    Working functions for summarising time and size of peak.

--- R/epidemic_peak.R
+++ R/epidemic_peak.R
@@ -3,6 +3,7 @@
 #' @param simulation_data Simulated epidemics from metapopulation SEIR model
 #'
 #' @return summary
+#' @importFrom rlang .data
 #' @export
 #'
 
@@ -11,6 +12,22 @@ epidemic_peak <- function(simulation_data) {
   # function that takes dataframe of simulations and extracts:
   # a) time of peak(median? range? 95% range?)
   # b) number of cases at peak (median? 95% range?)
+
+  # Use of .data in following code doesn;t seem to work
+  # peak <- simulation_data %>%
+  #   dplyr::filter(.data$variable == "infected") %>%
+  #   dplyr::group_by(.data$sim, .data$patch) %>%
+  #   dplyr::arrange(desc(.data$value)) %>%
+  #   dplyr::slice_head()
+
+  # peak_summary <- peak %>%
+  #   dplyr::group_by(.data$patch) %>%
+  #   dplyr::summarise(dplyr::tibble(peak_time_median = stats::median(.data$time),
+  #                                  peak_time_lo = stats::quantile(.data$time, 0.025),
+  #                                  peak_time_hi = stats::quantile(.data$time, 0.975),
+  #                                  peak_size_median = stats::median(.data$value),
+  #                                  peak_size_lo = stats::quantile(.data$value, 0.025),
+  #                                  peak_size_hi = stats::quantile(.data$value, 0.975)))
 
   peak <- simulation_data %>%
     dplyr::filter(variable == "infected") %>%
@@ -26,6 +43,8 @@ epidemic_peak <- function(simulation_data) {
                      peak_size_median = stats::median(value),
                      peak_size_lo = stats::quantile(value, 0.025),
                      peak_size_hi = stats::quantile(value, 0.975)))
+
+  peak_summary$patch <- forcats::as_factor(peak_summary$patch)
 
   peak_summary
 
---
+++ R/plot_peak_summary.R
@@ -0,0 +1,34 @@
+#' Plot summary of time and size of epidemic peak
+#'
+#' @param simulation_data Simulated epidemics from metapopulation SEIR model
+#'
+#' @return Plot summarising times and size of peaks in each patch
+#' @importFrom ggplot2 ggplot aes
+#' @export
+#'
+
+plot_peak_summary <- function(simulation_data) {
+
+  data_summary <- epidemic_peak(simulation_data)
+
+  times <-  ggplot(data_summary) +
+    ggplot2::geom_errorbar(aes(x = patch,
+                               ymin = peak_time_lo,
+                               ymax = peak_time_hi), width = 0.1) +
+    ggplot2::geom_point(aes(x = patch, y = peak_time_median)) +
+    ggplot2::xlab("Patch") +
+    ggplot2::ylab("Time of peak (days)") +
+    ggplot2::theme_classic()
+
+  size <- ggplot(data_summary) +
+    ggplot2::geom_errorbar(aes(x = patch,
+                               ymin = peak_size_lo,
+                               ymax = peak_size_hi), width = 0.1) +
+    ggplot2::geom_point(aes(x = patch, y = peak_size_median)) +
+    ggplot2::xlab("Patch") +
+    ggplot2::ylab("Number of cases at peak") +
+    ggplot2::theme_classic()
+
+  times + size
+
+}

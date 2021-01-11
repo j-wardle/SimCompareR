c2f301818621ef3e1725672244dcb604a81f016c
Jack Wardle <j.wardle19@imperial.ac.uk>
2021-01-11 12:56:23 +0000

    Add roxygen skeleton

--- R/epidemic_filter.R
+++ R/epidemic_filter.R
@@ -1,7 +1,16 @@
+#' Filter on whether any epidemic was observed in simulation.
+#'
+#' @param simulation_data Simulated epidemics from metapopulation SEIR model
+#'
+#' @return A subset of the simulation_data in which an epidemic was observed in at least one patch.
+#' @export
+#'
+
 epidemic_filter <- function(simulation_data) {
 
   epidemic <- epidemic_anywhere(simulation_data) %>%
     dplyr::filter(any_epi > 0)
 
+  epidemic
 
 }
